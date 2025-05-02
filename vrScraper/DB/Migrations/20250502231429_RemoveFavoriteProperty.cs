4335f2599f5dfe4477e14b971911611afd7436f1
schwoordd <sworddeamon@gmx.de>
2025-05-03 01:15:34 +0200

    Migration

---
+++ vrScraper/DB/Migrations/20250502231429_RemoveFavoriteProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace vrScraper.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveFavoriteProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Favorite",
+                table: "VideoItems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Favorite",
+                table: "VideoItems",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
